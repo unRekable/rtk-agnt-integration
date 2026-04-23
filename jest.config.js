5cb4a2b6acdaa07d54258fb64d071b3877fb8885
unRekable <61795925+unRekable@users.noreply.github.com>
2026-04-23 03:19:53 +0200

    test: add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  verbose: true
+};
